58f23550d8666b720a4c145073703b05a6edfd36
Shavkat Berdibekov <103271898+shavkat9@users.noreply.github.com>
2022-12-23 00:44:29 +0500

    Filter and forEach

---
+++ Filter method/app.js
@@ -0,0 +1,33 @@
+const numbers = [2, 3, 1, 7, 9, 4];
+
+let output = []
+
+numbers .forEach(item => {
+    if(item % 2 == 0)
+    output.push(item)
+});
+console.log(output);
+
+// ----------------------------------------------------
+
+let num = numbers.filter(item => {
+    return (item % 2 == 0)
+   
+});
+console.log(num);
+
+// ------------------------------------------------------
+
+const workers = [
+    {id: 53, name: 'Make Quin', age: 44, designation: 'tester', salary: 120000, tel: '+998993480829'},
+    {id: 36, name: 'Tom Sawyer', age: 34, designation: 'science', salary: 2110000, tel: '+99899358789'},
+    {id: 63, name: 'Cris Ron', age: 28, designation: 'tester', salary: 120000, tel: '+99899365843'},
+    {id: 45, name: 'Messi Leo', age: 41, designation: 'developer', salary: 190000, tel: '+998993480829'},
+    {id: 71, name: 'Golibboy', age: 39, designation: 'tester', salary: 120000, tel: '+998993897425'},
+    {id: 55, name: 'Mike Tom', age: 45, designation: 'computer', salary: 1440000, tel: '+99899369547'},
+    {id: 51, name: 'Melli Khan', age: 36, designation: 'tester', salary: 160000, tel: '+998993433254'}
+]
+
+console.log(
+    workers.filter(item => item.age < 29 && item.designation == 'tester')
+);
